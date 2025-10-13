d4b4578ea230ba15d1075d83af5fe5dba9f5c706
raorw <891770552@qq.com>
2025-10-13 11:51:00 +0800

    预加载图片和樱花

--- web/src/main.js
+++ web/src/main.js
@@ -11,8 +11,8 @@ const app = createApp(App)
 // 添加全局websocket
 app.config.globalProperties.$websocket = websocket
 // 连接WebSocket（根据你的实际地址修改）
-// websocket.connect('wss://74m4a28457.goho.co/enter')
-websocket.connect('ws://127.0.0.1:8000/enter')
+websocket.connect('wss://74m4a28457.goho.co/enter')
+// websocket.connect('ws://127.0.0.1:8000/enter')
 
 app.use(router) // 使用路由
 .mount('#app')
