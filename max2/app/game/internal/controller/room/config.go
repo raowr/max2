8d4a6f0255d19b65157500f9355c54451cb2de87
raorw <891770552@qq.com>
2025-10-20 08:12:22 +0800

    修改配置变量名

--- max2/app/game/internal/controller/room/config.go
+++ max2/app/game/internal/controller/room/config.go
@@ -7,10 +7,12 @@ import (
 )
 
 // 将全局变量改为函数，每次需要时重新从配置中获取
+// 最大出牌时间
 func GetOutCardTimeout() int {
-	return g.Cfg().MustGet(context.Background(), "game.outCard").Int() //出牌最大时间
+	return g.Cfg().MustGet(context.Background(), "game.outCardTimeout").Int() //出牌最大时间
 }
 
+// 每局抽水2(单位积分) /张
 func GetGameCommission() int64 {
 	return g.Cfg().MustGet(context.Background(), "game.comm").Int64() //每局抽水2
 }
