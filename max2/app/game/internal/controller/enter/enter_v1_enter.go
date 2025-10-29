40943b09fcb311469743c5db393c41e4e355bf38
raowenran <891770552@qq.com>
2025-10-29 15:04:46 +0800

    当删除房间时关闭旧的消息通道

--- max2/app/game/internal/controller/enter/enter_v1_enter.go
+++ max2/app/game/internal/controller/enter/enter_v1_enter.go
@@ -181,6 +181,7 @@ func (c *Client) handleInitRoom(ctx context.Context) {
 			if roomInfo.Rgtimer != nil {
 				roomInfo.Rgtimer.Stop()
 				roomInfo.Rgtimer.Close() // 停止定时器（确保资源释放）
+				close(roomInfo.MsgChan)  //关闭旧的消息通道
 			}
 			delete(rm.Rooms, oldRoomID)
 			g.Log().Infof(ctx, "用户 %s 清理旧房间: %s", c.userID, oldRoomID)
